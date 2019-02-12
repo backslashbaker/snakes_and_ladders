ec82e6c7f41fa12430da6b633b7bdc1d47020545
Derek <derek.baker89@gmail.com>
2019-02-12 20:09:51 +0000

    created logic to move up board

--- lib/board.rb
+++ lib/board.rb
@@ -1,5 +1,17 @@
 class Board
-	def make_move(position, marker)
-    0
-	end
+  def initialize
+    @positions = {}
+  end
+
+  def make_move(position, marker)
+    if @positions.key?(marker)
+      @positions[marker] = position + current_position(marker)
+    else
+      @positions[marker] = position
+    end
+  end
+
+  def current_position(marker)
+    @positions[marker]
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -1,8 +1,28 @@
-require_relative '../lib/board'
+require_relative "../lib/board"
 
 describe Board do
-  it 'moves 1 space when give 1' do
+  it "starts at position 0" do
     board = Board.new
-    expect(board.make_move(0, :x)).to eq 0
+
+    board.make_move(0, :x)
+
+    expect(board.current_position(:x)).to eq 0
+  end
+
+  it "moves 1 when given 1" do
+    board = Board.new
+
+    board.make_move(1, :x)
+
+    expect(board.current_position(:x)).to eq 1
+  end
+
+  it "gives correct position when making multiple moves" do
+    board = Board.new
+
+    board.make_move(1, :x)
+    board.make_move(5, :x)
+
+    expect(board.current_position(:x)).to eq 6
   end
 end
